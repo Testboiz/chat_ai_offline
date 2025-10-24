800cbe3f709645f29872964be588c48d800a4f9a
testboiz <tvmini0202@gmail.com>
2025-10-24 13:16:23 +0700

    test : complete tests and add mocks

---
+++ test/mocks/file_picker_mock.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:file_picker/file_picker.dart';
+
+@GenerateNiceMocks([MockSpec<FilePicker>()])
+void main() {}
---
+++ test/mocks/sqlite_database_mock.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+@GenerateNiceMocks([MockSpec<Database>()])
+void main() {}
